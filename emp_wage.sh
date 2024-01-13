1440728d6b09b4c90af5945448d9ec9daf0cfdeb
Stavin Alban <stavinalban@gmail.com>
2024-01-13 12:28:10 +0530

    Wage Calculated

--- emp_wage.sh
+++ emp_wage.sh
@@ -1,8 +1,4 @@
-isPresent=1
-checkAttendance=$((RAMDOM%2))
-if [ $isPresent -eq $checkAttendance ] 
-then 
-     echo "employee is present "
-else
-     echo "Absent"
-fi
+wage=20
+time=8
+dailywageperHR=$((wage*time))
+echo $dailywageperHR
