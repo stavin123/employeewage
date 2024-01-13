2a6fb53882c1bc8db6965df6832244e000790faf
Stavin Alban <stavinalban@gmail.com>
2024-01-13 12:52:47 +0530

    created switch case for calculating fulltime and parttime wages

--- emp_wage.sh
+++ emp_wage.sh
@@ -1,7 +1,16 @@
 wage=20
 fulltime=8
 halftime=4
-dailyfulltimewageperHR=$((wage*fulltime))
-dailyhalftimewageperHR=$((wage*halftime))
-echo $dailyfulltimewageperHR
-echo $dailyhalftimewageperHR
+echo "enter 1 for fulltime wage 2 for partime wage"
+read n
+case $n in
+  1)
+     dailyfulltimewageperHR=$((wage*fulltime))
+     echo $dailyfulltimewageperHR
+         ;;
+  2)
+      dailyhalftimewageperHR=$((wage*halftime)) 
+      echo $dailyhalftimewageperHR
+         ;;
+esac
+
